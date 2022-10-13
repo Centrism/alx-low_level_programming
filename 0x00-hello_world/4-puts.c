0f7ec7ca163c37cda3209643ea24b9a26bae5c53
Centrism <oluwaseunbdj@gmail.com>
2022-10-13 15:56:42 -0700

    100-intel

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *  main -Printing 
+ *  
+ *  Return: using puts function
+ */
+int main(void) 
+	{
+       	  puts("Programming is like building a multilingua
+        	 puzzle"\n);
+	return (0);
+	} 
