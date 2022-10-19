0055d3945171dd8f57d49bce22d12e4793373ac4
Centrism <oluwaseunbdj@gmail.com>
2022-10-19 16:03:41 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- * print_alphabet_x10 - print a - z ten times
- *
+ * main - print_alphabet_x10 - print a - z ten times
+ * Return (0) successful
  */
 
 void print_alphabet_x10(void)
