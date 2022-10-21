67a654ed3f4a54347a7649a4fc0b92d7a8ef53e5
Centrism <oluwaseunbdj@gmail.com>
2022-10-21 00:48:26 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  * print_triangle - Prints a triangle, using the character #.
+ *  main-  print_triangle - Prints a triangle, using the character #.
  *   * @size: The size of the triangle.
  */
 void print_triangle(int size)
